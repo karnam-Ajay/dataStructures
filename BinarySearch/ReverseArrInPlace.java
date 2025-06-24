2ee3eee097b85256761a701271877108b3e32847
karnam-Ajay <96416861+karnam-Ajay@users.noreply.github.com>
2025-06-24 16:24:00 +0000

    Reverse array in place

---
+++ BinarySearch/ReverseArrInPlace.java
@@ -0,0 +1,23 @@
+package BinarySearch;
+
+import java.util.Arrays;
+
+public class ReverseArrInPlace{
+    public static void main(String args[]){
+      int[] arr = {1,2,3,4,5};
+      ReverseArr(arr);
+      System.out.println(Arrays.toString(arr));
+    }
+    static void ReverseArr(int [] arr){
+        int start = 0;
+        int end = arr.length - 1;
+        while(start < end){
+           int temp = arr[start];
+           arr[start] = arr[end];
+           arr[end] = temp;
+
+           start++;
+           end--;
+        }
+    }
+}
