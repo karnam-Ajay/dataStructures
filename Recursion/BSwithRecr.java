5d10f6058bae30340c760768328a2ae809ef0a42
karnam-Ajay <96416861+karnam-Ajay@users.noreply.github.com>
2025-06-29 17:16:53 +0000

    BS with recursion

--- Recursion/BSwithRecr.java
+++ Recursion/BSwithRecr.java
@@ -1,5 +1,25 @@
 package Recursion;
 
 public class BSwithRecr {
-    
+    public static void main(String[] args) {
+        int[] num = {2,5,7,8,9,12,14};
+        System.out.println(binarySearchRecr(num, 11, 0, num.length-1));
+    }
+
+    static int binarySearchRecr(int[] arr, int target,int start,int end){
+
+        if (start > end) {
+            return -1;
+        }
+        int mid = start + (end - start)/2;
+
+        if (target == arr[mid]) {
+            return mid;
+        }
+
+        if (target > arr[mid]) {
+            return binarySearchRecr(arr, target, mid + 1, end);
+        }
+        return binarySearchRecr(arr, target, start, mid - 1);
+    }
 }
