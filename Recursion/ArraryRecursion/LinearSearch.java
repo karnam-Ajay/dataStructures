8f4bb20dd5efa4ad51c8941e77bac0f86e07d65a
karnam-Ajay <96416861+karnam-Ajay@users.noreply.github.com>
2025-07-05 08:19:42 +0000

    Linear Search with Recursion with general approach and using ArrayList in different ways

---
+++ Recursion/ArraryRecursion/LinearSearch.java
@@ -0,0 +1,80 @@
+package Recursion;
+
+import java.util.ArrayList;
+
+public class LinearSearch {
+    public static void main(String[] args) {
+        int[] arr = {1,3,4,4,6,7,9};
+        // System.out.println(linearSrch(arr, 8, 0));  //false
+        // System.out.println(linearSrchIdx(arr, 4, 0)); //2
+        // System.out.println(LinearSearchLast(arr, 6, arr.length - 1));  //3
+        // SearchMultipleIndexes(arr, 2, 0);
+        // System.out.println(list);
+        // System.out.println(SearchWithArrList(arr, 4, 0, new ArrayList<>())); // [2,3]
+    }
+
+    // Linear Search with Recursion
+    // to check target is there or not
+    static boolean linearSrch(int[] arr,int target,int index){
+        if (index == arr.length) {
+            return false;
+        }
+        return (arr[index] == target) || linearSrch(arr, target, index+1);
+    };
+
+    // To return the required target index
+    static int linearSrchIdx(int[] arr,int target,int index){
+        // if the target element is not there in the array just return -1
+        if (index == arr.length) {
+            return -1;
+        }
+
+        if(arr[index] == target){
+            return index;
+        }else{
+            return linearSrchIdx(arr, target, index + 1);
+        }
+    };
+
+    // To check from Last index
+
+    static int LinearSearchLast(int[] arr,int target,int index){
+        if (index == -1) {
+            return -1;
+        }
+
+        if (arr[index] == target) {
+            return index;
+        }else{
+            return LinearSearchLast(arr, target, index - 1);
+        }
+    }
+
+
+    // Find the index poisitons if target is present at more than one time
+   static ArrayList<Integer> list = new ArrayList<>();
+    static void SearchMultipleIndexes(int[] arr, int target,int index){
+        if (index == arr.length) {
+            return;
+        }
+
+        if (arr[index] == target) {
+            list.add(index);
+        }
+        SearchMultipleIndexes(arr, target, index + 1);
+    }
+
+    // By taking the list inside function argumet
+
+    static ArrayList<Integer> SearchWithArrList(int[] arr,int target,int index,ArrayList<Integer> list){
+        if (index == arr.length) {
+            return list;
+        }
+
+        if (arr[index] == target) {
+             list.add(index);
+        }
+        
+        return SearchWithArrList(arr, target, index + 1, list);
+    }
+}
